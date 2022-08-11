535deac238b5f7042ede81ba788e8a4b17044ad9
honsotuns <109986348+honsotuns@users.noreply.github.com>
2022-08-11 21:03:51 +0100

    test commit

--- manual.rps.py
+++ manual.rps.py
@@ -6,4 +6,4 @@ class RockPaper:
     def __init__(self, get_computer_choice, get_user_choice):
        self.get_computer_choice = get_computer_choice 
        self.get_user_choice = get_user_choice
-        
+    pass
