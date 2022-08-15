26f745264fae504a7216a297d870724276b41959
honsotuns <109986348+honsotuns@users.noreply.github.com>
2022-08-15 20:45:01 +0100

    added play game method

--- Hangman_project.py
+++ Hangman_project.py
@@ -3,6 +3,7 @@ import random
 
 class Hangman:
     
+    
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
--- manual.rps.py
+++ manual.rps.py
@@ -3,7 +3,66 @@ import random
 
 class RockPaper:
 
-    def __init__(self, get_computer_choice, get_user_choice):
+    def __init__(self, get_computer_choice, get_user_choice, get_winner, game ):
        self.get_computer_choice = get_computer_choice 
        self.get_user_choice = get_user_choice
-    pass
+       set.get_winner = get_winner
+       self.game = game
+
+
+    def get_computer_choice(self, computer_choice):
+        self.computer_choice = random.choice(computer_choice)
+        self.get_user_choice()
+    
+
+    def get_user_choice(self, user_choice):
+        self.get_user_choice = input (" Please choose Rock, Paper, or Scissors ").lower()
+        if self.user_choice not in self.game:
+            print (" Kindly choose among the options")
+        else:
+            self.get_winner()
+    
+def get_winner(self, get_winner):
+
+        print(f" You've chosen {self.get_user_choice}. The computer chose {self.computer_choice} ")
+        
+        if self.computer_choice  == 'rock':
+            if self.user_choice == 'paper':
+                print(' Congratulations, you won ')
+            elif self.user_choice == 'rock':
+                print("It's a draw, go again")
+            else:
+                print( 'You lost')
+        
+        if self.computer_choice == 'paper':
+            if self.user_choice == 'scissors':
+                print(' Congratulations, you won')
+            elif self.user_choice == 'paper':
+                print("it's a draw, go again ")
+            else:
+                print(' You lost')
+        
+        if self.computer_choice == 'scissors':
+            if self.user_choice == 'rock':
+                print(' Congratulations, you won') # I dont really know much about the games's rules, i just used my initiative
+            elif self.user_choice == 'scissors':
+                print(" it's a draw")
+            else:
+                print('You lost the game')
+    
+    #def play(self, play, game):
+       # self.play(game)
+
+                
+if __name__ == '__main__':
+    game_list = [' rock', 'paper', 'scissors' ]
+    get_winner(game_list)
+    
+    
+
+
+            
+
+
+
+    
